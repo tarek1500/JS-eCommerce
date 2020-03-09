28d7696d0fbbd46f38d109cf8f7117fa760beaa0
AliAhmedGomaa <alyahmedgomaa@gmail.com>
2020-03-09 22:09:07 +0200

    Contact Edits

--- js/contact.js
+++ js/contact.js
@@ -1,67 +1,97 @@
-
 let name = document.querySelector('#name'),
-email = document.querySelector('#email'),
-subject = document.querySelector('#subject'),
-message = document.querySelector('#message'),
-submit = document.querySelector('#btn'), 
-res = document.querySelector('#result'),
-nameError = document.querySelector('#nameError'),
-emailError = document.querySelector('#emailError'),
-subjectError = document.querySelector('#subjectError'),
-messageError = document.querySelector('#messageError');
+    email = document.querySelector('#email'),
+    subject = document.querySelector('#subject'),
+    message = document.querySelector('#message'),
+    submit = document.querySelector('#btn'), 
+    res = document.querySelector('#result'),
+    nameError = document.querySelector('#nameError'),
+    emailError = document.querySelector('#emailError'),
+    subjectError = document.querySelector('#subjectError'),
+    messageError = document.querySelector('#messageError');
 
-submit.addEventListener('click', (ev) => {
 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-var urlencoded = new URLSearchParams();
-urlencoded.append("name", name.value);
-urlencoded.append("email", email.value);
-urlencoded.append("subject", subject.value);
-urlencoded.append("message", message.value);
 
-var requestOptions = {
-method: 'POST',
-headers: myHeaders,
-body: urlencoded,
-redirect: 'follow'
-};
+function sendData()
+{
 
-fetch("https://afternoon-falls-30227.herokuapp.com/api/v1/contact_us", requestOptions)
-.then(response => response.text())
-.then(result => {
-result = JSON.parse(result)
-res.textContent = result.message;
-if(result.error){
-    if (result.error.name){
-      nameError.textContent = result.error.name;
-    }
-    else{
-      nameError.textContent = '';
-    }
-    if (result.error.email){
-      emailError.textContent = result.error.email;
-    }
-    else{
-      emailError.textContent = '';
+    const data = {
+        name: name.value,
+        email: email.value,
+        subject: subject.text,
+        message: message.text
     }
 
-    if(subject.value == ''){
-      subjectError.textContent = "subject is required";
-    }
-    else{
-      subjectError.textContent = '';
-    }
+    
+    console.log(data.subject);
+    
+    const xhr = new XMLHttpRequest();
 
-    if(message.value == ''){
-      messageError.textContent = "message is required";
-    }  else{
-      messageError.textContent = '';
-    }
+    xhr.open('POST', 'https://afternoon-falls-30227.herokuapp.com/api/v1/contact_us',);
+
+    xhr.responseType = 'json';
+  
+      if (data) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
+
+
+      xhr.onload = () => {
+            if (xhr.status >= 400) {
+                console.log(xhr.response);
+
+                if(xhr.response.error.name){
+                    nameError.textContent = xhr.response.error.name;
+                }else{
+                    nameError.textContent = ''
+                }
+
+                if(xhr.response.error.email){
+                    emailError.textContent = xhr.response.error.email;
+                }else{
+                    emailError.textContent = ''
+                }
+
+                if(xhr.response.error.subject){
+                    subjectError.textContent = xhr.response.error.subject;
+                }else{
+                    subjectError.textContent = ''
+                }
+
+                if(xhr.response.error.message){
+                    messageError.textContent = xhr.response.error.message;
+                }else{
+                    messageError.textContent = ''
+                }
+            } else {
+                res.textContent = xhr.response.message
+                console.log(xhr.response);
+                
+            }
+            };
+        
+            xhr.onerror = () => {
+            console.log('Erooooor');
+        };
+
+      xhr.send(JSON.stringify(data))
+
+
 }
 
-})
+submit.addEventListener('click', sendData);
 
 
-})
+//       xhr.onload = () => {
+//         if (xhr.status >= 400) {
+//           reject(xhr.response);
+//         } else {
+//           resolve(xhr.response);
+//         }
+//       };
+  
+//       xhr.onerror = () => {
+//         reject('Erooooor');
+//       };
+  
+//       xhr.send(JSON.stringify(data))
