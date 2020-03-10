19c42f83068dc3b27998eb6931da235c7d64ddf9
rola281997 <rolaahmedabdelmaksoud281997@gmail.com>
2020-03-10 06:08:47 +0200

    pure js

--- js/product.js
+++ js/product.js
@@ -2,11 +2,12 @@
 //queryString = queryString.substring(1);
 //var url = 'https://afternoon-falls-30227.herokuapp.com/api/v1/products/HT-5000' ;
 //+ queryString;
-var queryString = decodeURIComponent(window.location.search); //parsing
-queryString = queryString.substring(1);
+//var queryString = decodeURIComponent(window.location.search); //parsing
+//queryString = queryString.substring(1);
+var queryString = 'HT-1000';
 var url = 'https://afternoon-falls-30227.herokuapp.com/api/v1/products/' + queryString;
 
-    $.getJSON(url, function(data) {
+/*    $.getJSON(url, function(data) {
 
         var product_id = `ProductID: ${data.data.ProductId}`;
 		
@@ -63,4 +64,87 @@ var url = 'https://afternoon-falls-30227.herokuapp.com/api/v1/products/' + query
         $("#section").hide();
     
     
-    });
+    });*/
+	
+	
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    myFunction(myArr);
+  }else if(this.status == 404){
+	var error_msg = `data not found` 
+    $("#error").html(error_msg);
+    $("#section").hide(); 
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.onerror = function () {
+
+};
+xmlhttp.send();
+
+function myFunction(arr) {
+	console.log(arr);
+	console.log("test1",arr.data);
+         var category =`${arr.data.Category}` 
+		 $("#Category").html(category);
+		 
+		 var MainCategory =`${arr.data.MainCategory}` 
+		 $("#MainCategory").html(MainCategory);
+		 
+		 var TaxTarifCode =`${arr.data.TaxTarifCode}` 
+		 $("#TaxTarifCode").html(TaxTarifCode);
+	     var SupplierName =`${arr.data.SupplierName}` 
+		 $("#SupplierName").html(SupplierName);
+		  var WeightMeasure =`${arr.data.WeightMeasure}` 
+		 $("#WeightMeasure").html(WeightMeasure);
+		 
+		  var WeightUnit =`${arr.data.WeightUnit}` 
+		 $("#WeightUnit").html(WeightUnit);
+		 var Description =`${arr.data.Description}` 
+		 $("#description").html(Description);
+		  var Name=`${arr.data.Name}` 
+		 $("#name").html(Name);
+		  
+		  var DateOfSale =`${arr.data.DateOfSale}` 
+		 $("#DateOfSale").html(DateOfSale);
+		  var Status =`${arr.data.Status}` 
+		 $("#Status").html(Status);
+		 
+		   var Quantity =`${arr.data.Quantity}` 
+		 $("#Quantity").html(Quantity);
+		   var UoM =`${arr.data.UoM}` 
+		 $("#UoM").html(UoM);
+		   var CurrencyCode =`${arr.data.CurrencyCode}` 
+		 $("#CurrencyCode").html(CurrencyCode);
+		   var Price =`${arr.data.Price}` 
+		 $("#prices").html(Price);
+		   var Width =`${arr.data.Width}` 
+		 $("#Width").html(Width);
+		   var Depth =`${arr.data.Depth}` 
+		 $("#Depth").html(Depth);
+		   var Height =`${arr.data.Height}` 
+		$("#Height").html(Height);
+		var DimUnit =`${arr.data.DimUnit}` 
+		$("#DimUnit").html(DimUnit);
+		 var ProductpicUrl =`${arr.data.ProductPicUrl}` 
+		 $( "#product-img" ).attr('src', ProductpicUrl);
+		$("#error").hide();
+  }
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
