d0e8a1d89d1e2fe1f86d4c5955421cad8f6bff7c
rola281997 <rolaahmedabdelmaksoud281997@gmail.com>
2020-03-12 18:54:27 +0200

    finished

--- js/product.js
+++ js/product.js
@@ -1,21 +1,82 @@
-//var queryString = decodeURIComponent(window.location.search); //parsing
-//queryString = queryString.substring(1);
-//var url = 'https://afternoon-falls-30227.herokuapp.com/api/v1/products/HT-5000' ;
-//+ queryString;
-var queryString = decodeURIComponent(window.location.search); //parsing
+var stored_qty;
+var quantity;
+var calculated_quantity;
+var myArr;
+var statusError;
+var error_msg ;
+var queryString;
+queryString = decodeURIComponent(window.location.search); //parsing
 queryString = queryString.substring(1);
-var queryString = 'HT-1002';
 var url = 'https://afternoon-falls-30227.herokuapp.com/api/v1/products/' + queryString;
+
+if(!queryString){
+    
+    error_msg = `data not found`;
+	document.getElementById("error").innerHTML=error_msg;
+    document.getElementById("section").style.display = "none";
+    
+}
+else{
+//get quantity from database
+//prefixes of implementation that we want to test
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+ 
+//prefixes of window.IDB objects
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+ 
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.");
+}
+var db;
+var request = window.indexedDB.open("cart", 1);
+request.onerror = function(event) {
+	console.log("error: ");
+};
+request.onsuccess = function(event) {
+	db = request.result;
+	console.log("success: "+ db);
+};
+
+function read() {
+    var transaction = db.transaction(["Cart_Orders"],"readonly");
+    var objectStore = transaction.objectStore("Cart_Orders");
+    var index = objectStore.index("prod_idx");
+    var request = index.get(queryString);
+    request.onerror = function(event) {
+       console.log("Unable to retrieve daa from database!");
+    };
+    request.onsuccess = function(event) {
+		console.log(request);
+        if(request.result) {
+            console.log("PRODUCT_ID:",request.result.prod_id);
+			console.log("Quantity",request.result.quantity);
+			stored_qty = request.result.quantity;
+			console.log("stored_qty",stored_qty);
+			calculated_quantity = quantity - stored_qty;
+			console.log("Quantity",quantity);
+			console.log("calculated_quantity",calculated_quantity);
+			document.getElementById("Quantity").innerHTML = calculated_quantity;
+        } else {
+            console.log("no quantity be found in your database!"); 
+			
+			document.getElementById("Quantity").innerHTML = quantity;
+        }
+    };
+}
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
+  statusError = this.status;
   if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
     myFunction(myArr);
+      	read();
   }else if(this.status == 404){
-	var error_msg = `data not found` 
+	error_msg = `data not found` 
    
 	document.getElementById("error").innerHTML =error_msg;
-   // $("#section").hide(); 
+  
     document.getElementById("section").style.display = "none";
   }
 };
@@ -23,7 +84,8 @@ xmlhttp.open("GET", url, true);
 xmlhttp.onerror = function () {
 
 };
-xmlhttp.send();
+xmlhttp.send(); 
+
 
 function myFunction(arr) {
 	    console.log(arr);
@@ -50,8 +112,8 @@ function myFunction(arr) {
 		
 		  var Status =obj.Status; 
 		 document.getElementById("Status").innerHTML = Status;
-		  var Quantity =obj.Quantity;
-		 document.getElementById("Quantity").innerHTML = Quantity;
+		   quantity =obj.Quantity;
+		 
 		   var CurrencyCode =obj.CurrencyCode; 
 		 
 		   var Price =obj.Price;
@@ -70,13 +132,13 @@ function myFunction(arr) {
 		document.getElementById("Volume").innerHTML = volumeunit;
 		var ProductPicUrl =obj.ProductPicUrl; 
 		 document.getElementById("product-img").src = ProductPicUrl;
-		//$("#error").hide();
+		
 		  document.getElementById("error").style.display = "none";
 		
 		
   }
 
-
+}
 
 
 
--- js/shop.js
+++
@@ -1,3 +0,0 @@
-var id = 'HT-1000';
-var queryString = "?" + id;
-window.location.href = "product.html" + queryString;
